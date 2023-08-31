6c1f52ae5fd2a82ec4b5d94667c08c5d725e718f
Rodrigo <r.principe@reply.it>
2023-08-31 19:08:28 +0200

    boilerplate spring initializer

---
+++ src/main/java/com/reply/pay/fabric/fabricMiddleware/FabricMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.reply.pay.fabric.fabricMiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reply/pay/fabric/fabricMiddleware/FabricMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reply.pay.fabric.fabricMiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
