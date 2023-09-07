7782d178828d3c29ad88b2b30ab24f4306758674
Rodrigo <r.principe@reply.it>
2023-09-07 21:16:08 +0200

    add moneyTransfer mapping and transactions mapping to MainController plus refactor RestTemplateBuilder

--- src/main/java/com/reply/pay/fabrick/fabrickMiddleware/MainController.java
+++ src/main/java/com/reply/pay/fabrick/fabrickMiddleware/MainController.java
@@ -3,28 +3,42 @@ package com.reply.pay.fabrick.fabrickMiddleware;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.result.method.annotation.RequestMappingHandlerMapping;
 
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("/bankAccount")
 public class MainController {
     @Value("${spring.application.name}")
     String appName;
 
-    String baseUrl = "https://sandbox.platfr.io";
-    String path = "/api/gbs/banking/v4.0/accounts";
-    String url = baseUrl + path;
+    private final String API_KEY = "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP";
+    private final String baseUrl = "https://sandbox.platfr.io";
+    private final String path = "/api/gbs/banking/v4.0/accounts";
+    private final String downstreamUrl = baseUrl + path;
 
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public MainController(RestTemplateBuilder builder) {
+        this.restTemplate = builder.errorHandler(new CustomResponseErrorHandler())
+                .defaultHeader("Auth-Schema", "S2S")
+                .defaultHeader("Api-Key", API_KEY)
+                .build();
+
+    }
 
     @GetMapping("/")
     public String homePage(Model model) {
@@ -32,66 +46,66 @@ public class MainController {
         return "home";
     }
 
-    @GetMapping("/home")
-    public String homePageUri(Model model) {
-        model.addAttribute("appName", appName);
-        return "homeUri";
-    }
+    @GetMapping("/{accountId}/balance")
+    public ResponseEntity<?> balance(@PathVariable String accountId, HttpServletRequest httpServletRequest, RequestMappingHandlerMapping mapping) throws JsonProcessingException {
 
-    @GetMapping("/balance/{accountId}")
-    public ResponseEntity<?> balance(@PathVariable String accountId) throws JsonProcessingException {
-
-        String balanceUrl = url + "/" + accountId + "/balance";
-
-        RestTemplateBuilder builder = new RestTemplateBuilder();
-        String API_KEY = "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP";
-        RestTemplate restTemplate = builder
-                .errorHandler(new CustomResponseErrorHandler())
-                .defaultHeader("Auth-Schema", "S2S")
-                .defaultHeader("Api-Key", API_KEY)
-                .build();
-
-//        RestTemplate restTemplate = new RestTemplate();
+        String balanceUrl = downstreamUrl + "/" + accountId + "/balance";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.add("X-Time-Zone", "'Europe/Rome'");
-//        headers.add("Auth-Schema", "S2S");
-//        headers.add("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
-//        headers.add("accountId","145377820");
 
         HttpEntity<String> request = new HttpEntity<>(headers);
-//                new HttpEntity<String>(personJsonObject.toString(), headers);
-
-//        String result = restTemplate.getForObject(balanceUrl, String.class);
-//        String result = restTemplate.getForObject(balanceUrl, String.class);
-//                restTemplate.postForObject(createPersonUrl, request, String.class);
-//        restTemplate.setErrorHandler(new CustomResponseErrorHandler());
 
         ResponseEntity<String> response = restTemplate.exchange(balanceUrl, HttpMethod.GET, request, String.class);
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getBody()));
+
         String httpBodyResponse = Objects.requireNonNull(response.getBody()).lines()
                 .collect(Collectors.joining(""));
 
 //        JsonNode root = objectMapper.readTree(result);
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode httpBodyResponseJson = mapper.readTree(httpBodyResponse);
-        String responseBody = httpBodyResponseJson.get("payload").toString();
+        JsonNode httpBodyResponseJsonNode = mapper.readTree(httpBodyResponse);
+        String responseBody = httpBodyResponseJsonNode.get("payload").toString();
 
         LinkedMultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
         response.getHeaders().toSingleValueMap().forEach(multiValueMap::add);
-        ResponseEntity responseEntity =  new ResponseEntity<>(responseBody, multiValueMap, response.getStatusCode());
 
-        return responseEntity;
+        return new ResponseEntity<>(responseBody, multiValueMap, response.getStatusCode());
     }
 
-    @GetMapping("/ok")
-    public ResponseEntity<?> okay() {
+    @GetMapping("/{accountId}/transactions")
+    public ResponseEntity<?> transactions(@PathVariable String accountId,
+                                          @RequestParam String fromAccountingDate,
+                                          @RequestParam String toAccountingDate) throws JsonProcessingException {
 
-        return new ResponseEntity<>("okay", HttpStatus.BAD_REQUEST);
+        String transactionsUrl = downstreamUrl + "/" + accountId + "/" + "transactions"
+                + "?" + "fromAccountingDate=" + fromAccountingDate + "&toAccountingDate=" + toAccountingDate;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(transactionsUrl, HttpMethod.GET, request, String.class);
+
+        String httpBodyResponse = Objects.requireNonNull(response.getBody()).lines()
+                .collect(Collectors.joining(""));
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode httpBodyResponseJsonNode = mapper.readTree(httpBodyResponse);
+        String responseBody = httpBodyResponseJsonNode.get("payload").toString();
+
+        LinkedMultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
+        response.getHeaders().toSingleValueMap().forEach(multiValueMap::add);
+
+        return new ResponseEntity<>(responseBody, multiValueMap, response.getStatusCode());
     }
 
+    @PostMapping("/{accountId}/payments/moneyTransfers")
+    public ResponseEntity<?> transactions(@PathVariable String accountId) {
+        String moneyTransferUrl = downstreamUrl + "/" + accountId + "/" + "/payments/money-transfers";
+        return null;
 
+    }
 //    @GetMapping("/balance2")
 //    public ResponseEntity<?> balance2() {
 //        String balanceUrl = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts";
